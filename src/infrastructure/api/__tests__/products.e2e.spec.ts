a909652774c4a303a77bfe609ce19615fe8c5f2d
Ine Ballardin <ineballardin@gmail.com>
2024-01-01 16:50:32 -0300

    refactor: :recycle: Refactor code and add test to products.presenter.ts
    Refactor the following files: products.e2e.spec.ts, products.presenter.ts, products.route.ts
    
    Related to issue: #7

--- src/infrastructure/api/__tests__/products.e2e.spec.ts
+++ src/infrastructure/api/__tests__/products.e2e.spec.ts
@@ -1,8 +1,15 @@
 import { app, sequelize } from "../express"
 import request from "supertest";
+import ProductsPresenter from "../presenters/products.presenter";
+import { OutputListProductsDto } from "../../../usecase/product/list/list.product.dto";
 
-describe("E2E test for list products", () => {
-    beforeEach(async () => {
+const PRODUCT_NAME_A = "Product A";
+const PRODUCT_PRICE_A = 12.90;
+const PRODUCT_NAME_B = "Product B";
+const PRODUCT_PRICE_B = 21.90;
+
+describe("E2E test for products functionalities", () => {
+    beforeAll(async () => {
         await sequelize.sync({ force: true });
     });
 
@@ -10,43 +17,36 @@ describe("E2E test for list products", () => {
         await sequelize.close();
     });
 
-    it("should create a product", async () => {
-        const response = await request(app)
+    const createProduct = async (name: string, price: number) => {
+        return await request(app)
             .post("/products")
             .send({
-                name: "Product A",
-                price: 31.50,
+                name: name,
+                price: price,
             });
+      };
 
+    it("should create products successfully", async () => {
+        let response = await createProduct(PRODUCT_NAME_A, PRODUCT_PRICE_A);
         expect(response.status).toBe(200);
         expect(response.body.name).toBe("Product A");
-        expect(response.body.price).toBe(31.50);
+        expect(response.body.price).toBe(12.90);
+
+        response = await createProduct(PRODUCT_NAME_B, PRODUCT_PRICE_B);
+        expect(response.status).toBe(200);
+        expect(response.body.name).toBe("Product B");
+        expect(response.body.price).toBe(21.90);
     });
 
-    it("should not create a product", async () => {
+    it("should fails to create a product without price", async () => {
         const response = await request(app).post("/products").send({
             name: "Product A",
         });
         expect(response.status).toBe(500)
+        expect(response.body.error).toBe("Price is required");
     });
 
     it("should list all products", async () => {
-        const responseProductA = await request(app)
-            .post("/products")
-            .send({
-                name: "Product A",
-                price: 12.90,
-            });
-        expect(responseProductA.status).toBe(200);
-
-        const responseProductB = await request(app)
-            .post("/products")
-            .send({
-                name: "Product B",
-                price: 21.90,
-            });
-        expect(responseProductB.status).toBe(200);
-
         const listProductsResponse = await request(app).get("/products").send();
         expect(listProductsResponse.status).toBe(200);
         expect(listProductsResponse.body.products.length).toBe(2);
@@ -78,3 +78,27 @@ describe("E2E test for list products", () => {
         expect(listProductsResponseXML.text).toContain(`</products>`);
     });
 });
+
+describe('ProductsPresenter', () => {
+    it('should convert product data to XML correctly', () => {
+        const mockData: OutputListProductsDto = {
+            products: [
+                { id: "1", name: 'Product A', price: 12.90 },
+                { id: "2", name: 'Product B', price: 21.90 },
+            ],
+        };
+        const expectedXml = `<?xml version="1.0" encoding="UTF-8"?>
+<products>
+  <product>
+    <id>1</id>
+    <name>Product A</name>
+    <price>12.9</price>
+    <id>2</id>
+    <name>Product B</name>
+    <price>21.9</price>
+  </product>
+</products>`; 
+        const result = ProductsPresenter.listXML(mockData);
+        expect(result).toEqual(expectedXml);
+    });
+ });
