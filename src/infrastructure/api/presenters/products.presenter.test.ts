a19b5bcc4f06c81e1ad6ecfa922049356f30ed4f
Ine Ballardin <ineballardin@gmail.com>
2024-01-04 18:07:23 -0300

    test: :test_tube: Add unit tests for ProductsPresenter listXML method

---
+++ src/infrastructure/api/presenters/products.presenter.test.ts
@@ -0,0 +1,63 @@
+import { OutputListProductsDto } from "../../../usecase/product/list/list.product.dto";
+import ProductsPresenter from "./products.presenter";
+
+
+jest.mock('jstoxml', () => ({
+    toXML: jest.fn()
+}));
+
+describe('ProductsPresenter', () => {
+    describe('listXML method', () => {
+        it('should correctly call toXML with the right parameters and return XML', () => {
+            const mockToXML = require('jstoxml').toXML;
+            const testData: OutputListProductsDto = {
+                products: [
+                    { id: '1', name: 'Product 1', price: 100 },
+                    { id: '2', name: 'Product 2', price: 200 }
+                ]
+            };
+
+            const expectedXML = `<xml><products><product><id>1</id><name>Product 1</name><price>100</price></product><product><id>2</id><name>Product 2</name><price>200</price></product></products></xml>`;
+            mockToXML.mockReturnValue(expectedXML);
+
+            const result = ProductsPresenter.listXML(testData);
+            
+            const expectedJSONPassedToToXML = {
+                products: {
+                    product: [
+                        { id: '1', name: 'Product 1', price: 100 },
+                        { id: '2', name: 'Product 2', price: 200 }
+                    ]
+                }
+            };
+
+            expect(mockToXML).toHaveBeenCalledWith(expectedJSONPassedToToXML, {
+                header: true,
+                indent: "  ",
+                newLine: "\n",
+                allowEmpty: true,
+            });
+            
+            expect(result).toBe(expectedXML);
+        });
+
+        it('should throw an error if toXML throws an error', () => {
+            const mockToXML = require('jstoxml').toXML;
+            const testData: OutputListProductsDto = {
+                products: [
+                    { id: '1', name: 'Product 1', price: 100 },
+                    { id: '2', name: 'Product 2', price: 200 }
+                ]
+            };
+
+            const testError = new Error('Test error');
+            mockToXML.mockImplementation(() => {
+                throw testError;
+            });
+
+            expect(() => {
+                ProductsPresenter.listXML(testData);
+            }).toThrow(testError);
+        });
+    });
+});
